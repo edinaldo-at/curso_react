a5f693461233be374295c1230cca56ce2e52e0fe
Edinaldo Alves <edinaldo.at@gmail.com>
2021-06-07 12:18:46 -0300

    aula 14:  Interando uma array utilizando MAP

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -1,130 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-//Exemplo statefull  -  forma longa, antiga
+
 class App extends Component {
-
-  //Exemplo com BIND
-  /*
-  constructor(props) {
-    super(props)
-    //lembrete: feito o BIND para que possamos usar o THIS no metodo handlePclick mesmo ele estando dentro da mesma classe
-    this.handlePclick = this.handlePclick.bind(this);
-    this.state = {
-      name: "Edinaldo Alves",
-      counter: 0
-    };
-  }
-  */
-
-  //Exemplo do contrutor sem o Bind usando aerofunctions
-  /*
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: "Edinaldo Alves",
-      counter: 0
-    };
-  }*/
-
   //class fields
   state = {
-    name: "Edinaldo Alves",
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'o título 1',
+        body: 'o corpo 1'
+      },
+      {
+        id: 2,
+        title: 'o título 2',
+        body: 'o corpo 2'
+      },
+      {
+        id: 3,
+        title: 'o título 3',
+        body: 'o corpo 3'
+      }
+    ]
   };
 
-  //Exemplo Sem contrutor
-
-  handlePclick = () => {
-    //const { name } = this.state;
-    //console.log(`<p>Clicado - ${name}</p>`)
-
-    //Mudando o estado utilizando o metodo setState
-    this.setState({ name: 'Cláudio' });
-  }
-
-  //lembrete: metodo como areofunction -  não é necessário fazer o BIND
-  handleAclick = (event) => {
-    event.preventDefault();
-
-    const { counter } = this.state;
-
-    this.setState({ counter: counter + 1 });
-  }
-
   render() {
-    //lembrete: pegue a chave nome que está dentro de state
-    const { name, counter } = this.state;
+
+    //pega posts de dentro de state
+    const { posts } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePclick}>
-            {name} - {counter}
-          </p>
-          <a
-            onClick={this.handleAclick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Clique-Me
-          </a>
-        </header>
+        {/*Exemplo: map retornando apenas um elemento
+        posts.map(post => <h1 key={post.id}>{post.title}</h1>)*/}
+
+        {/*Exemplo: interando multiplos elementos*/}
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <span>{post.body}</span>
+          </div>
+        ))}
+
       </div>
     );
   };
 };
 
-
-//class stateless (Sem estado)
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-*/
-//function stateless (Sem estado)
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
